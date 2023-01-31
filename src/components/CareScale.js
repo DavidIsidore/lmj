292cc411ae9f624e8a4cf0e981d1d33cdd6c5edb
David Isidore <isidoredavid@protonmail.com>
2023-01-31 17:00:42 +0100

    Correction de l'exercice

--- src/components/CareScale.js
+++ src/components/CareScale.js
@@ -1,18 +1,16 @@
 import Sun from '../assets/sun.svg'
 import Water from '../assets/water.svg'
 
+// Ici, il s'agit d'une manière de faire.
+//Vous auriez aussi pu utiliser une fonction qui retourne l'élément souhaité, ou bien faire directement des conditions
+const quantityLabel = {
+	1: 'peu',
+	2: 'modérément',
+	3: 'beaucoup'
+}
+
 function CareScale({ scaleValue, careType }) {
 	const range = [1, 2, 3]
-
-	function handleClickWater(water) {
-		alert(`Cette plante requiert ${range.map((rangeElem) =>
-			scaleValue >= rangeElem ? (
-				<span key={rangeElem.toString()}>{scaleType}</span>
-			) : null
-		)}`)
-		}
-
-		
 	const scaleType =
 		careType === 'light' ? (
 			<img src={Sun} alt='sun-icon' />
@@ -21,19 +19,22 @@ function CareScale({ scaleValue, careType }) {
 		)
 
 	return (
-		<div onClick={() => handleClickWater(Water)}>
+		<div
+			onClick={() =>
+				alert(
+					`Cette plante requiert ${quantityLabel[scaleValue]} ${
+						careType === 'light' ? 'de lumière' : "d'arrosage"
+					}`
+				)
+			}
+		>
 			{range.map((rangeElem) =>
 				scaleValue >= rangeElem ? (
 					<span key={rangeElem.toString()}>{scaleType}</span>
 				) : null
 			)}
-			
 		</div>
 	)
 }
-
-
-
-
 
 export default CareScale
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,15 +3,16 @@ import '../styles/Footer.css'
 
 function Footer() {
 	const [inputValue, setInputValue] = useState('')
-    //const isValid = inputValue.includes('@')
 
-    function checkValue(value) {
-        if(inputValue.includes('@')) {
-            setInputValue(value)
-        } else {
-            alert('Attention, il n\'y a pas d\'@, ceci n\'est pas une adresse valide')
-        }
-    }
+	function handleInput(e) {
+		setInputValue(e.target.value)
+	}
+
+	function handleBlur() {
+		if (!inputValue.includes('@')) {
+			alert("Attention, il n'y a pas d'@, ceci n'est pas une adresse valide 😥")
+		}
+	}
 
 	return (
 		<footer className='lmj-footer'>
@@ -19,14 +20,14 @@ function Footer() {
 				Pour les passionné·e·s de plantes 🌿🌱🌵
 			</div>
 			<div className='lmj-footer-elem'>Laissez-nous votre mail :</div>
-            <div>
-                <input type="email" value={inputValue}
-                onChange={(e) => {setInputValue(e.target.value)}}
-                onClick={(e) => {checkValue(e.target.value)}} />
-            </div>
+			<input
+				placeholder='Entrez votre mail'
+				onChange={handleInput}
+				value={inputValue}
+				onBlur={handleBlur}
+			/>
 		</footer>
 	)
 }
-
 
 export default Footer
